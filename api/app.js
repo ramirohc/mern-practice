1d22f102028571bb31b4e343ea136e3c03d88f13
Hibraim <hibraimmartinez@gmail.com>
2022-09-03 09:26:26 -0600

    Changes of Variables
    the changes have been delivered

--- api/app.js
+++ api/app.js
@@ -15,24 +15,24 @@ const verifyToken = (rq, res, next) =>{
     const token = authHeader && authHeader.split('')[1];
     console.log(authHeader);
     if(token=null)
-     return res.status(401).send("Token requerido");
+     return res.status(401).send("Required Token");
     jwt.verify(token, TOKEN_KEY, (err, user)=>{
-        if(err) return res.status(403).send("Token invalido");
+        if(err) return res.status(403).send("Invalid Token");
         console.log(user);
         req.user = user;
         next();
     });
 }
 
-app.post("/usuario/login", (req,res)=>{
-    const usuario = req.body.usuario;
-    const clave = req.body.clave;
-    if(usuario=='fmendoza' && clave=='123456'){
+app.post("/username/login", (req,res)=>{
+    const name = req.body.name;
+    const code = req.body.code;
+    if(name=='fmendoza' && code=='123456'){
         const datos={
                 id:"123",
-                nombre: "Felipe Mendoza",
+                name: "Felipe Mendoza",
                 email: "fmendoza@mail.com",
-                codigo: "ABDE456-lk"
+                code: "ABDE456-lk"
         };
         const token = jwt.sign(
             {userID:datos.id, email:datos.email},
@@ -42,20 +42,20 @@ app.post("/usuario/login", (req,res)=>{
         let nDatos = {...datos, token};
         res.status("200").json(datos);
     }else{
-        res.status(400).send("Crendenciales incorrectas");
+        res.status(400).send("Wrong credentials");
     }
 });
 
 app.get("/usaurio/:id/ventas", verifyToken, (req, res)=>{
     const datos = [
-        {id:1,cliente:"Empresa A",total:2500,fecha:"2022-01-15"},
-        {id:2,cliente:"Empresa B",total:2100,fecha:"2022-01-15"},
-        {id:3,cliente:"Empresa B",total:2200,fecha:"2022-01-15"},
+        {id:1,client:"Business A",total:2500,date:"2022-01-15"},
+        {id:2,client:"Business B",total:2100,date:"2022-01-15"},
+        {id:3,client:"Business B",total:2200,date:"2022-01-15"},
 
     ];
     res.json(datos);
 });
 
 app.listen(3001, ()=>{
-    console.log("Servidor iniciado en el puerto 3001");
+    console.log("Server Started 3001");
 })
